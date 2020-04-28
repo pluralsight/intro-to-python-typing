17a857dc15ac232667963fc43c30c3a1e7c2bd2a
David Sturgis <david@smarterer.com>
2020-04-28 09:13:45 -0400

    Add missing annotation in example 03
    Add str type to `text` variable

--- examples/03_tuple_types.py
+++ examples/03_tuple_types.py
@@ -3,7 +3,7 @@ The Tuple type is useful for describing return values
 """
 from typing import Tuple
 
-def first_word_length(text) -> Tuple[str, int]:
+def first_word_length(text: str) -> Tuple[str, int]:
     word, *_ =  text.split(' ')
     return word, len(word)
 
